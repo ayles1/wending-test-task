efe13ccf6d547d894d02512dc424529c90f217fe
ayles <ayles1113@gmail.com>
2023-05-04 18:47:09 +0300

    fix: move wrappers in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import store from '@/store';
+import { Provider } from 'react-redux';
+
 import Home from '@/pages/Home';
 
 import '@/styles/reset.scss';
 
 function App() {
-    return <Home></Home>;
+    return (
+        <Provider store={store}>
+            <Home></Home>;
+        </Provider>
+    );
 }
 
 export default App;
